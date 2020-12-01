affc1b712fa92a0fb8d5a214368b68734736cf5b
Griffin Surett <leveloutusa@gmail.com>
2020-12-01 12:31:07 -0500

    jj

---
+++ database.sql
@@ -0,0 +1,34 @@
+DROP DATABASE IF EXISTS employeeTracker;
+CREATE database employeeTracker;
+USE employeeTracker;
+
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(30),
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+  id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(30),
+  salary INT,
+  department_id INT,
+  FOREIGN KEY (department_id) REFERENCES department(id),
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  role_id INT,
+  FOREIGN KEY (role_id) REFERENCES role(id),
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE manages (
+employee_id INT not null,
+manager_id INT not null, 
+FOREIGN KEY (employee_id) REFERENCES employee(id),
+FOREIGN KEY (manager_id) REFERENCES employee(id)
+);
